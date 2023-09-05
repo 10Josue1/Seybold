246dfcaa9f380c834a7dfddf16eb7d9fc0b324c9
10Josue1 <92957443+10Josue1@users.noreply.github.com>
2023-09-05 15:23:27 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let boton = document.getElementById("icono");
+let enlaces = document.getElementById("enlaces");
+let contador = 0;
+
+boton.addEventListener("click", function(e){
+    e.preventDefault();
+    if(contador === 0){
+        enlaces.classList.remove("uno");
+        enlaces.classList.add("dos");
+        contador = 1;
+    } else {
+        enlaces.classList.remove("dos");
+        enlaces.classList.add("uno");
+        contador = 0;
+    }
+});
